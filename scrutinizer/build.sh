3d285f552ac303d75e8a12abd8fdd9a4db310ba8
Wilhelm Kärde <wilhelm@Willes-PC.(none)>
2014-05-15 17:11:43 +0200

    Updated the rules library, the README file as well as the build and setEnv scripts

--- scrutinizer/build.sh
+++ scrutinizer/build.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-make clean
 make depend
 make scrut/rules.tab.h
 make all
--- scrutinizer/setEnv.sh
+++ scrutinizer/setEnv.sh
@@ -2,12 +2,14 @@
 
 echo "Setting environment variables for Granska"
 echo ""
-export TAGGER_LEXICON= /granska/lex
+export GRANSKA_HOME=~/Skola/Exjobb/granska/
+echo "GRANSKA_HOME = "$GRANSKA_HOME
+export TAGGER_LEXICON=~/Skola/Exjobb/granska/lex
 echo "TAGGER_LEXICON = "$TAGGER_LEXICON
-export STAVA_LEXICON= /granska/stava/lib/
+export STAVA_LEXICON=~/Skola/Exjobb/granska/stava/lib/
 echo "STAVA_LEXICON = " $STAVA_LEXICON
-export SCRUTINIZER_RULE_FILE= /granska/regler/regelsamling.ver8
+export SCRUTINIZER_RULE_FILE=~/Skola/Exjobb/granska/rulesets/wille/regelsamling.ver8
 echo "SCRUTINIZER_RULE_FILE = "$SCRUTINIZER_RULE_FILE
-export SCRUTINIZER_TEST_TEXT= /granska/regler/regelsamling.ver8.testfil
+export SCRUTINIZER_TEST_TEXT=~/Skola/Exjobb/granska/rulesets/wille/regelsamling.ver8.testfil
 echo "SCRUTINIZER_TEST_TEXT = "$SCRUTINIZER_TEST_TEXT
 echo "Done"
